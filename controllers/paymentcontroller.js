f6ac598560ddc52a93f391485804799a0a9d20ce
Aleeeeen <youremail@example.com>
2025-03-24 10:58:06 +0530

    Updated backend changes

--- controllers/paymentcontroller.js
+++ controllers/paymentcontroller.js
@@ -11,8 +11,8 @@ exports.createPayment = async (req, res) => {
         intent: "sale",
         payer: { payment_method: "paypal" },
         redirect_urls: {
-            return_url: "http://localhost:3000/success",
-            cancel_url: "http://localhost:3000/cancel"
+            return_url: "https://befitbackend.onrender.com/success",
+            cancel_url: "https://befitbackend.onrender.com/cancel"
         },
         transactions: [{
             amount: { currency: "USD", total: amount },
